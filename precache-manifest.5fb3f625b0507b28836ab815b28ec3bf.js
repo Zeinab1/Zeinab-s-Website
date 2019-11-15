fccaf9858f8fcd12baa4cf55561f74034710eee5
Zeinab Gamal <zeinab.gamal512@gmail.com>
2019-11-15 17:46:10 +0200

    Updates

---
+++ precache-manifest.5fb3f625b0507b28836ab815b28ec3bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4672108af2f866c1c8ccfbdd4b1499ee",
+    "url": "/Zeinab-s-Website/index.html"
+  },
+  {
+    "revision": "bf7d23cfed5d82de6d09",
+    "url": "/Zeinab-s-Website/static/css/main.e77ef652.chunk.css"
+  },
+  {
+    "revision": "d44f4428e609eed7654f",
+    "url": "/Zeinab-s-Website/static/js/2.3afaadf0.chunk.js"
+  },
+  {
+    "revision": "bf7d23cfed5d82de6d09",
+    "url": "/Zeinab-s-Website/static/js/main.c71959cc.chunk.js"
+  },
+  {
+    "revision": "2963e7de922cbf6d563c",
+    "url": "/Zeinab-s-Website/static/js/runtime-main.d794432b.js"
+  },
+  {
+    "revision": "62108e0d97d82e5f48151500b07f0926",
+    "url": "/Zeinab-s-Website/static/media/developer.62108e0d.png"
+  }
+]);
